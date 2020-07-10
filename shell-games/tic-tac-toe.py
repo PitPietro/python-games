bc411823373daa5a7ded7cb35b3dfb54d48b1a5b
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-07-10 12:23:51 +0200

    Tic-Tac-Toe number system

--- shell-games/tic-tac-toe.py
+++ shell-games/tic-tac-toe.py
@@ -8,16 +8,32 @@ import os
 
 def welcome_print():
     welcome = ' Welcome to Tic-Tac-Toe Game by PitPietro '
-    w_len = len(welcome)
     cols = get_terminal_dim()[1]
-    tot_len = int(cols) - w_len
-    half_len = tot_len // 2
+    half_w_len = (int(cols) - len(welcome)) // 2
+    ten_s = ' '
+    for i in range(10):
+        ten_s += ' '
     print_hash(int(cols))
     print()
-    print_hash(half_len)
+    print_hash(half_w_len)
     print(welcome, end='')
-    print_hash(half_len + 1)
-    print()
+    print_hash(half_w_len)
+    print(end='\n\n')
+    print(ten_s, 'Number system is given below:', end='\n\n')
+    line_l = ' 1 | 2 | 3 '
+    dashes = ' --------- '
+    half_l_len = (int(cols) - len(line_l)) // 2
+    print_spaces(half_l_len)
+    print(line_l)
+    print_spaces(half_l_len)
+    print(dashes)
+    print_spaces(half_l_len)
+    print(' 4 | 5 | 6 ')
+    print_spaces(half_l_len)
+    print(dashes)
+    print_spaces(half_l_len)
+    print(' 7 | 8 | 9 ', end='\n\n')
+
 
 
 def get_terminal_dim():
@@ -32,6 +48,11 @@ def tic_tac_toe_game():
 def print_hash(number):
     for i in range(number):
         print('#', end='')
+
+
+def print_spaces(number):
+    for i in range(number):
+        print(' ', end='')
 
 
 tic_tac_toe_game()
