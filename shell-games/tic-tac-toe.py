14f9b3448008817d495e241279ffd122996333a8
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-07-11 18:45:33 +0200

    print defs generalization

--- shell-games/tic-tac-toe.py
+++ shell-games/tic-tac-toe.py
@@ -13,31 +13,27 @@ def welcome_print():
     welcome = ' Welcome to Tic-Tac-Toe Game by PitPietro '
     cols = get_terminal_dim()[1]
     half_w_len = (int(cols) - len(welcome)) // 2
-    print_hash(int(cols))
+    print_symbol('#', int(cols))
     print()
-    print_hash(half_w_len)
+    print_symbol('#', half_w_len)
     print(welcome, end='')
-    print_hash(half_w_len)
+    print_symbol('#', half_w_len)
     print(end='\n\n')
     print(ten_s, 'Number system is given below:', end='\n\n')
     line_l = ' 1 | 2 | 3 '
     dashes = ' --------- '
     half_l_len = (int(cols) - len(line_l)) // 2
-    print_spaces(half_l_len)
+    print_symbol(' ', half_l_len)
     print(line_l)
-    print_spaces(half_l_len)
+    print_symbol(' ', half_l_len)
     print(dashes)
-    print_spaces(half_l_len)
+    print_symbol(' ', half_l_len)
     print(' 4 | 5 | 6 ')
-    print_spaces(half_l_len)
+    print_symbol(' ', half_l_len)
     print(dashes)
-    print_spaces(half_l_len)
+    print_symbol(' ', half_l_len)
     print(' 7 | 8 | 9 ', end='\n\n')
     print(ten_s, 'Let\'s start the game!', end='\n\n')
-
-
-def print_current_status():
-    print_hash(int(get_terminal_dim()[1]))
     
 
 def get_terminal_dim():
@@ -55,18 +51,17 @@ def get_player_input():
         return False
 
 
+def print_current_status():
+    pass
+
+
 def tic_tac_toe_game():
     welcome_print()
 
 
-def print_hash(number):
+def print_symbol(symbol, number):
     for i in range(number):
-        print('#', end='')
-
-
-def print_spaces(number):
-    for i in range(number):
-        print(' ', end='')
+        print(symbol, end='')
 
 
 tic_tac_toe_game()
