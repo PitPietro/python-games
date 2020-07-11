f43ce45a8ffafcddb8c12cc73acb8caac5b7e117
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-07-11 19:20:32 +0200

    print_current_status part

--- shell-games/tic-tac-toe.py
+++ shell-games/tic-tac-toe.py
@@ -34,7 +34,7 @@ def welcome_print():
     print_symbol(' ', half_l_len)
     print(' 7 | 8 | 9 ', end='\n\n')
     print(ten_s, 'Let\'s start the game!', end='\n\n')
-    
+
 
 def get_terminal_dim():
     rows, columns = os.popen('stty size', 'r').read().split()
@@ -52,16 +52,45 @@ def get_player_input():
 
 
 def print_current_status():
-    pass
+    welcome = ' Current status of the board '
+    cols = get_terminal_dim()[1]
+    half_w_len = (int(cols) - len(welcome)) // 2
+    print_symbol('#', int(cols))
+    print()
+    print_symbol('#', half_w_len)
+    print(welcome, end='')
+    print_symbol('#', half_w_len)
+    print(end='\n\n')
 
+    line_l = ' {} | {} | {}'
+    dashes = ' --------- '
+    half_l_len = (int(cols) - len(line_l)) // 2
 
-def tic_tac_toe_game():
-    welcome_print()
+    msg = print_symbol_return(' ', half_l_len) + line_l + '\n' + \
+          print_symbol_return(' ', half_l_len) + dashes + '\n' + \
+          print_symbol_return(' ', half_l_len) + line_l + '\n' + \
+          print_symbol_return(' ', half_l_len) + dashes + '\n' + \
+          print_symbol_return(' ', half_l_len) + line_l
+    print(msg.format(*board))
+
+    print(end='\n\n')
 
 
 def print_symbol(symbol, number):
     for i in range(number):
         print(symbol, end='')
+
+
+def print_symbol_return(symbol, number):
+    msg = ''
+    for i in range(number):
+        msg += symbol
+    return msg
+
+
+def tic_tac_toe_game():
+    print_current_status()
+    welcome_print()
 
 
 tic_tac_toe_game()
